f91c55a732300342b0e6b99d380918568492ecbb
luan <luan.vieira@zup.com.br>
2019-06-13 16:11:25 -0300

    Create modules

---
+++ src/app/modules/attended/attended.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attended',
+  templateUrl: './attended.component.html',
+  styleUrls: ['./attended.component.scss']
+})
+export class AttendedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

5ab9d6aa6bdd946c7e6c65a95d93f192fe33cca3
luan <luan.vieira@zup.com.br>
2019-06-13 16:14:03 -0300

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AttendedComponent } from './modules/attended/attended.component';
+import { TrashComponent } from './modules/trash/trash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AttendedComponent,
+    TrashComponent
   ],
   imports: [
     BrowserModule,

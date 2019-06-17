ac60678d4531c6b879398f45876e40f325839ba1
luan <luan.vieira@zup.com.br>
2019-06-17 14:18:46 -0300

    Header layout

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
 
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    SideBarComponent
   ]
 })
 export class CommonModule { }
